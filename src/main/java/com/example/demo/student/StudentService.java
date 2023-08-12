b4c47760b45010578d520a785dc722dba44dd62f
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-08-12 05:34:06 +0100

    setting the properties file

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,8 +1,7 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
