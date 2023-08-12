7101c0510ba57ab527148158d2e9ca66d228938d
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-08-12 04:12:21 +0100

    Student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +18,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping
-	public String hello(){
-		return "Hello World ";
+	public List<Student> hello(){
+		return List.of(new Student(1L,
+				"soufiane",
+				"erraoudisoufiane5@gmail.com",
+				LocalDate.of(2002, Month.JUNE,24),
+				21));
 	}
 
 }
