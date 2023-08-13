a24fcf81302e2f5da6ebf0c4b331870a9c06b97e
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-08-13 04:17:47 +0100

    JPA Repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
@@ -8,11 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L,
-                "soufiane",
-                "erraoudisoufiane5@gmail.com",
-                LocalDate.of(2002, Month.JUNE, 24),
-                21));
+        return studentRepository.findAll();
     }
 }
