a6c0be9984ec0bc028b45cec4a90e386b4f70247
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-08-16 05:58:38 +0100

    post Mapping

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,8 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -19,5 +21,11 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
     }
 }
